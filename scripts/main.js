1ed0b5e5fab9d6f0292e1b3ae5e21d50ae72ba63
Max Comstock <maxibillion6@gmail.com>
2020-10-21 23:37:16 -0400

    Add shader for running simulations and use data

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,188 @@
 /* global require */
 require([
   'libs/Abubu.js',
+  'text!data/zebrafish_onecl.txt',
+  'text!shaders/run_simulation.frag',
 ], function(
   Abubu,
+  ActualData,
+  RunSimulationShader,
 ) {
   'use strict';
 
+  //
+  // Get canvas information
+  //
+
   var canvas_1 = document.getElementById('canvas_1');
+  var particles_width = parseInt(canvas_1.getAttribute('width'));
+  var particles_height = parseInt(canvas_1.getAttribute('height'));
+
+  var num_particles = particles_width * particles_height;
+
+  var bounds = {
+    trmin: 25,
+    trmax: 200,
+    tsimin: 10,
+    tsimax: 300,
+    twpmin: 50,
+    twpmax: 500,
+    tdmin: 0.15,
+    tdmax: 0.4,
+    tvpmin: 1,
+    tvpmax: 20,
+    tv1mmin: 10,
+    tv1mmax: 50,
+    tv2mmin: 500,
+    tv2mmax: 1500,
+    twmmin: 5,
+    twmmax: 100,
+    tomin: 5,
+    tomax: 50,
+    xkmin: 1,
+    xkmax: 15,
+    ucsimin: 0.2,
+    ucsimax: 0.9,
+    ucmin: 0.1,
+    ucmax: 0.25,
+    uvmin: 0.005,
+    uvmax: 0.05,
+  };
+
+  //
+  // Set up data used to evaluate the each simulation run
+  //
+
+  var actual_data = ActualData.split('\n');
+  var data_length = actual_data.length;
+  var data_array = new Float32Array(data_length*4);
+
+  // Pad out the extra pixel values. The data could be stored more densely by using the full pixel
+  // value and by using a two-dimensional texture, but for now there is not enough to require that.
+  var p = 0;
+  for (var i = 0; i < data_length; ++i) {
+    data_array[p++] = parseFloat(actual_data[i].trim());
+    data_array[p++] = 0.0;
+    data_array[p++] = 0.0;
+    data_array[p++] = 0.0;
+  }
+
+  var data_texture = new Abubu.Float32Texture(data_length, 1, {
+    pairable: true,
+    data: data_array,
+  });
+
+  //
+  // Environment for solver with default values
+  //
+
+  var env = {
+    simulation: {
+      dt: 0.02,
+      period: 400.0,
+      stim_start: 2.0,
+      stim_end: 7.0,
+      stim_mag: 0.1,
+      num_beats: 1,
+      v_init: 1.0,
+      w_init: 1.0,
+    },
+  };
+
+  //
+  // Simulation running solver
+  //
+
+  // Textures to store the values for each particle. There are so many dimensions this needs to be
+  // split into multiple textures.
+  var particles_texture_1 = new Abubu.Float32Texture(particles_width, particles_height, {
+    pariable: true,
+  });
+  var particles_texture_2 = new Abubu.Float32Texture(particles_width, particles_height, {
+    pariable: true,
+  });
+  var particles_texture_3 = new Abubu.Float32Texture(particles_width, particles_height, {
+    pariable: true,
+  });
+  var particles_texture_4 = new Abubu.Float32Texture(particles_width, particles_height, {
+    pariable: true,
+  });
+
+  // Texture to store the error value returned from one run of the simulation by the corresponding
+  // particle
+  var error_texture = new Abubu.Float32Texture(particles_width, particles_height, {
+    pariable: true,
+  });
+
+  // Solver for running a simulation once for each particle
+  var run_simulations = new Abubu.Solver({
+    fragmentShader: RunSimulationShader,
+    uniforms: {
+      in_particles_1: {
+        type: 't',
+        value: particles_texture_1,
+      },
+      in_particles_2: {
+        type: 't',
+        value: particles_texture_2,
+      },
+      in_particles_3: {
+        type: 't',
+        value: particles_texture_3,
+      },
+      in_particles_4: {
+        type: 't',
+        value: particles_texture_4,
+      },
+      data_texture: {
+        type: 't',
+        value: data_texture,
+      },
+      dt: {
+        type: 'f',
+        value: env.simulation.dt,
+      },
+      period: {
+        type: 'f',
+        value: env.simulation.period,
+      },
+      stim_start: {
+        type: 'f',
+        value: env.simulation.stim_start,
+      },
+      stim_end: {
+        type: 'f',
+        value: env.simulation.stim_end,
+      },
+      stim_mag: {
+        type: 'f',
+        value: env.simulation.stim_mag,
+      },
+      num_beats: {
+        type: 'i',
+        value: env.simulation.num_beats,
+      },
+      v_init: {
+        type: 'f',
+        value: env.simulation.v_init,
+      },
+      w_init: {
+        type: 'f',
+        value: env.simulation.w_init,
+      },
+    },
+    targets: {
+      error_texture: {
+        location: 0,
+        target: error_texture,
+      },
+    },
+  });
+
+  //
+  // Remaining work:
+  // * Actually run the simulation solver
+  // * Error aggregation to find global best
+  // * Particle updates and local best tracking
+  //
 });
---
+++ shaders/run_simulation.frag
@@ -0,0 +1,112 @@
+#version 300 es
+
+precision highp float;
+precision highp int;
+
+uniform sampler2D in_particles_1, in_particles_2, in_particles_3, in_particles_4;
+uniform sampler2D data_texture;
+
+layout (location = 0) out vec4 error_texture;
+
+in vec2 cc;
+
+// Simulation parameters
+uniform float dt, period, stim_start, stim_end, stim_mag;
+uniform int num_beats;
+uniform float v_init, w_init;
+
+// Macros to get the particles from the textures
+#define TR_POS particles_1.r
+#define TSI_POS particles_1.g
+#define TWP_POS particles_1.b
+#define TD_POS particles_1.a
+
+#define TVP_POS particles_2.r
+#define TV1M_POS particles_2.g
+#define TV2M_POS particles_2.b
+#define TWM_POS particles_2.a
+
+#define TO_POS particles_3.r
+#define XK_POS particles_3.g
+#define UCSI_POS particles_3.b
+#define UC_POS particles_3.a
+
+#define UV_POS particles_4.r
+
+// Useful functions
+#define TANH(x) ((exp(2.0*(x)) - 1.0) / (exp(2.0*(x)) + 1.0))
+
+void main() {
+    // PSO derived parameters
+    int num_period = int(ceil(period/dt));
+    float endtime = ceil(float(num_beats)*period);
+    int num_steps = int(ceil(endtime/dt));
+
+    // Get the relevant color from each texture
+    vec4 particles_1 = texture(in_particles_1, cc);
+    vec4 particles_2 = texture(in_particles_2, cc);
+    vec4 particles_3 = texture(in_particles_3, cc);
+    vec4 particles_4 = texture(in_particles_4, cc);
+
+    // Initialize values for the simulation
+    float u = 0.0;
+    float v = v_init;
+    float w = w_init;
+
+    float error = 0.0;
+
+    int data_index = 0;
+
+    // Run the simulation with the current swarm parameters
+    for (int step_count = 0; step_count < num_steps; ++step_count) {
+        float p = u >= UC_POS ? 1.0 : 0.0;
+        float q = u >= UV_POS ? 1.0 : 0.0;
+
+        float dv =
+            // p false
+            (1.0 - p)*(1.0 - v) / (
+                // q false
+                (1.0 - q)*TV1M_POS
+                // q true
+                + q*TV2M_POS
+                )
+            // p true
+            - p*(v/TVP_POS);
+
+        float dw =
+            // p false
+            (1.0-p)*(1.0-w) / TWM_POS
+            // p true
+            - p*(w/TWP_POS);
+
+        v += dt*dv;
+        w += dt*dw;
+
+        float jfi =
+            // p true
+            p * -v * (u - UC_POS)*(1.0-u)/TD_POS;
+
+        float jso =
+            // p false
+            (1.0 - p)*u / TO_POS
+            // p true
+            // TODO: Ask Elizabeth if there should be a u term here
+            + p/TR_POS;
+
+        float jsi = -w * (1.0 + TANH(XK_POS * (u-UCSI_POS))) / (2.0 * TSI_POS);
+
+        // Apply stimulus
+        float stim = 0.0;
+        if (mod(float(step_count), period/dt) < stim_start/dt && mod(float(step_count), period/dt) > stim_end/dt) {
+            stim = stim_mag;
+        }
+
+        u -= (jfi+jso+jsi-stim)*dt;
+
+        // Measure error
+        if (mod(float(step_count), round(1.0/dt)) == 0.0) {
+            float actual = texelFetch(data_texture, ivec2(data_index++, 0), 0).r;
+            error += (u - actual)*(u - actual);
+        }
+    }
+}
